34f18d1548023b57b83a9eb9fc4eea2f8bf59580
Wenmeng Liu <wenmengliu@uvic.ca>
2021-03-11 22:46:17 -0800

    LC_986

---
+++ leetcode/intervals_tag/986-Interval-List-Intersections/986-Interval-List-Intersections.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        # two pointers
+        i, j, ans = 0, 0, []
+        
+        while i < len(firstList) and j < len(secondList):
+            s = max(firstList[i][0], secondList[j][0])
+            e = min(firstList[i][1], secondList[j][1])
+            
+            if s<= e:
+                ans.append([s,e])
+            if firstList[i][1] < secondList[j][1]:
+                i += 1
+            else:
+                j += 1
+        
+        return ans
