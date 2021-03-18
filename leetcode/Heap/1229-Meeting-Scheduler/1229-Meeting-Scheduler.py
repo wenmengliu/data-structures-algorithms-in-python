dc7a07558a8858c32f5ea4cf4eefe488629f5c83
Wenmeng Liu <wenmengliu@uvic.ca>
2021-03-17 23:24:56 -0700

    LC_1229

---
+++ leetcode/Heap/1229-Meeting-Scheduler/1229-Meeting-Scheduler.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        timeslots = list(filter(lambda x: x[1] - x[0] >= duration, slots1 + slots2))
+        heapq.heapify(timeslots)
+        print(timeslots)
+        
+        while len(timeslots) > 1:
+            s1, e1 = heapq.heappop(timeslots)
+            s2, e2 = timeslots[0]
+            
+            if e1 >= s2 + duration:
+                return [s2, s2 + duration]
+        
+        return []
