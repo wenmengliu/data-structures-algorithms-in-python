c50717e4122d61a56cd72e58729d88ffc52aaf64
Wenmeng Liu <wenmengliu@uvic.ca>
2021-06-19 16:01:39 -0700

    LC_1130 mono_stack

---
+++ leetcode/Stack/1130-Minimum-Cost-Tree-From-Leaf-Values/1130-Minimum-Cost-Tree-From-Leaf-Values.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        stack = [float('inf')]
+        res = 0
+        for num in arr:
+            while stack and stack[-1] <= num:
+                cur = stack.pop()
+                res += cur * min(stack[-1], num)
+            stack.append(num)
+        while len(stack) > 2:
+            res += stack.pop() * stack[-1]
+        return res
