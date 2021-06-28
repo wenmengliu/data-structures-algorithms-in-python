ddf4411ade3cc4a1b5a6c4ae1095230c7c692d26
Wenmeng Liu <wenmengliu@uvic.ca>
2021-06-28 15:07:44 -0700

    LC_80 Two pointers

---
+++ leetcode/Two-Pointer/80-Remove-Duplicates-from-Sorted-Array-II/80-Remove-Duplicates-from-Sorted-Array-II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return len(nums)
+        
+        slow, fast = 2, 2
+        while fast < n:
+            if nums[slow - 2] < nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
