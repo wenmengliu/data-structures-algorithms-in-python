24918b1602583a1e2a1847db24827e978eb33689
Wenmeng Liu <wenmengliu@uvic.ca>
2021-07-03 13:37:30 -0700

    LC_206 Linked-List prev-curr-next pointer

---
+++ leetcode/Linked-List/206-Reverse-Linked-List/206-Reverse-Linked-List.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        while head:
+            nextt = head.next
+            head.next = prev
+            prev = head
+            head = nextt
+        return prev
