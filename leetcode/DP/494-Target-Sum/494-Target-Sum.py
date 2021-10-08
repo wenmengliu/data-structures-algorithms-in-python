c7f32103f14edce40eaadae735866f698ade21a5
Wenmeng Liu <wenmengliu@uvic.ca>
2021-10-07 22:09:14 -0700

    LC_494 0-1 knapsack trick

--- leetcode/DP/494-Target-Sum/494-Target-Sum.py
+++ leetcode/DP/494-Target-Sum/494-Target-Sum.py
@@ -1,24 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        ssum = sum(nums)
-        if ssum < target:
-             return 0
+        # 0-1 knapsack
+        total = sum(nums)
+        if total < abs(target) or (total + target)%2:
+            return 0
         
-        kMaxN = 2 * ssum + 1
-        kOffset = ssum
-        dp = [0] * kMaxN
-        dp[kOffset] = 1
+        capacity = (total + target)//2
+        dp = [0] * (capacity + 1)
+        dp[0] = 1
         
         for num in nums:
-            temp = [0] * kMaxN
-            for i in range(num, kMaxN - num):
-                if dp[i]:
-                    temp[i + num] += dp[i]
-                    temp[i - num] += dp[i]
-            temp, dp = dp, temp
-        
-        return dp[kOffset + target]
-            
+            for j in range(capacity, num - 1, -1):
+                dp[j] = dp[j] + dp[j - num]
+        return dp[capacity]
+       
 # TC: O(n * sum)
 # SC: O(sum) 
-# trasfter function: dp[i][j] += dp[i-1][j-nums[i]] or dp[i][j] += dp[i-1][j+nums[i]]
+# trasfter function: dp[i][j] = dp[i-1][j] + dp[i-1][j-num]
+# dp[i][j] means for the first ith items to get capacity j
