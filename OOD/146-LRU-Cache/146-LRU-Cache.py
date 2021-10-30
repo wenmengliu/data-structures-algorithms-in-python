38c38f680dfb1c4fc3a4d9746a821a71c381259d
Wenmeng Liu <wenmengliu@uvic.ca>
2021-10-30 11:08:46 -0700

    updt

--- OOD/146-LRU-Cache/146-LRU-Cache.py
+++ OOD/146-LRU-Cache/146-LRU-Cache.py
@@ -1,63 +1,60 @@
+class DLL:
+    
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.head = DLinkedNode(0,0)
-        self.tail = DLinkedNode(0,0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.dict = {}
-
-    def get(self, key: int) -> int:
-        if key in self.dict:
-            node = self.dict[key]
-            # remove an node from the existing Linked list
-            self._remove(node)
-            # move an evicted node after head
-            self._add(node)
-            return node.value
-        return -1
+        self.cap = capacity
+        self.cache = {}
+        self.dh = DLL(0,0)
+        self.dt = DLL(-1, -1)
+        self.dh.next = self.dt
+        self.dt.prev = self.dh 
         
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self.removeFromList(node)
+            self.insertIntoHead(node)
+            return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self._remove(self.dict[key])
-        node = DLinkedNode(key, value)
-        self._add(node)
-        self.dict[key] = node
-        if len(self.dict) > self.capacity:
-            n = self.head.next
-            self._remove(n)
-            del self.dict[n.key]
+        # update the value similiar to get()
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.removeFromList(node)
+            self.insertIntoHead(node)
+        else:
+            if len(self.cache) >= self.cap:
+                self.removeFromTail()
+            node = DLL(key, value)
+            self.cache[key] = node
+            self.insertIntoHead(node)
     
+    def insertIntoHead(self, node):
+        head_next = self.dh.next 
+        self.dh.next = node
+        node.prev = self.dh
+        node.next = head_next
+        head_next.prev = node 
     
-    def _remove(self, node):
-        p = node.prev 
-        n = node.next 
-        p.next = n
-        n.prev = p
+    def removeFromList(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
     
-    
-    def _add(self, node):
-        p = self.tail.prev 
-        p.next = node
-        self.tail.prev = node
-        node.next = self.tail
-        node.prev = p
-
-class DLinkedNode():
-    
-    def __init__(self, k, v):
-        self.key = k
-        self.value = v
-        self.prev = None
-        self.next = None
-
-# HashTable + DoubleLinkedList to achieve put and get in O(1) time
-# Access to a random key in O(1) -> hashtable
-# remove the first entry in LRU cache in O(1) -> list
-# add/move an entry to the end of LRU cache in O(1) -> list
-
+    def removeFromTail(self):
+        if len(self.cache) == 0:
+            return
+        tail_node = self.dt.prev 
+        del self.cache[tail_node.key]
+        self.removeFromList(tail_node)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
